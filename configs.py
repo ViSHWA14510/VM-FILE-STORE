b1284642fcfaa888284c46b2e5471c4d0b627bbd
ViSHWA <154061854+ViSHWA14510@users.noreply.github.com>
2024-09-28 01:01:15 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,20 +1,20 @@
 import os
 
 class Config(object):
-  API_ID = int(os.environ.get("API_ID", ""))
-  API_HASH = os.environ.get("API_HASH", "")
-  BOT_TOKEN = os.environ.get("BOT_TOKEN", "6919202862:AAG_6HCNZavbdRah9Nmfm7MjV0ccChq6wHA")
+  API_ID = int(os.environ.get("API_ID", "27344784"))
+  API_HASH = os.environ.get("API_HASH", "a12613f54c74d00bfb76cf8f0688c2ee")
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "6919202862:AAEAGnJdJcqTjM9XjldhqbvF_THySUAz2HE")
   BOT_USERNAME = os.environ.get("BOT_USERNAME", "VM_FILE_STORE_BOT")
   DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1002056408106"))
   SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "https://publicearn.com")
   SHORTLINK_API = os.environ.get('SHORTLINK_API', "548469c58f85d19221472ee23951d8bc910507bd")
   BOT_OWNER = int(os.environ.get("BOT_OWNER", "6221524623"))
-  DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Cluster0:Cluster0@cluster0.ystkyjc.mongodb.net/?retryWrites=true&w=majority")
+  DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://alibaba0:alibaba0@alibaba0.61vkq.mongodb.net/?retryWrites=true&w=majority")
   UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001811300784")
   LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002048118352"))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
   FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
-  BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
+  BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
   BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "").split()))
   OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
   ABOUT_BOT_TEXT = f"""
